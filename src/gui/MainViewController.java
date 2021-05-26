1bc494c314dc6bc6c5f0d42666d055a463a4b520
Julia Mayumi <juliamaytm@gmail.com>
2021-05-26 15:14:27 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -17,87 +18,73 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
 
-public class MainViewController implements Initializable{
-	
+public class MainViewController implements Initializable {
+
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
-	private MenuItem menuItemAbout;	
-	
+	private MenuItem menuItemAbout;
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			//a partir do objeto loader vc pode carregar a view e acessar o controller(referência para o controller dessa view)
+			controller.setDepartmenteService(new DepartmentService()); //Injetar a dependência do service no controller
+			controller.updateTableView(); //Com o controller já com depedência é só chamar o método par atualizar os dados na table view
+		});
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rs) {
 	}
 
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
-			
-			//Método para mostrar a view dentro da janela principal
-			//Para trabalhar com a janela principal é necessário uma referência da cena 
+
+			// Método para mostrar a view dentro da janela principal
+			// Para trabalhar com a janela principal é necessário uma referência da cena
 			Scene mainScene = Main.getMainScene();
-			
-			/*Pq precisou de 1 referência pra cena?
-			ScrollPane > content > VBox > children
-			A ideia é acrescentar em children de Vbox os childrens do VBox da janela 'About'
-			É necessário uma referência ao VBox da janela principal */
-			
-			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent(); 
-			//Método getRoot() pega o primeiro elemento da View(que é o scrollpane)
-			//Método getContent() acessa o 'content' que tá abaixo da hierarquia de ScrollPane
-			//'content' já é uma referência pra o que está dentro do ScrollPane(como o VBox), por isso o casting '(VBox)'
-			
-			Node mainMenu = mainVBox.getChildren().get(0); //Primeiro filho do VBox da janela principal
-			mainVBox.getChildren().clear(); //Limpa todos os filhos do mainVBox
+
+			/*
+			 * Pq precisou de 1 referência pra cena? ScrollPane > content > VBox > children
+			 * A ideia é acrescentar em children de Vbox os childrens do VBox da janela
+			 * 'About' É necessário uma referência ao VBox da janela principal
+			 */
+
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			// Método getRoot() pega o primeiro elemento da View(que é o scrollpane)
+			// Método getContent() acessa o 'content' que tá abaixo da hierarquia de
+			// ScrollPane
+			// 'content' já é uma referência pra o que está dentro do ScrollPane(como o
+			// VBox), por isso o casting '(VBox)'
+
+			Node mainMenu = mainVBox.getChildren().get(0); // Primeiro filho do VBox da janela principal
+			mainVBox.getChildren().clear(); // Limpa todos os filhos do mainVBox
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			
-			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent(); 
-			
-			Node mainMenu = mainVBox.getChildren().get(0); 
-			mainVBox.getChildren().clear(); 
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			//a partir do objeto loader vc pode carregar a view e acessar o controller(referência para o controller dessa view)
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmenteService(new DepartmentService()); //Injetar a dependência do service no controller
-			controller.updateTableView(); //Com o controller já com depedência é só chamar o método par atualizar os dados na table view
-			
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
+
 }
